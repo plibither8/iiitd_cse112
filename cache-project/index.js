e710c22ac704c289041f31495da91fe8b56821ab
Mihir Chaturvedi <plibither8@gmail.com>
2020-06-16 23:02:01 +0530

    Docs: Update README

--- cache-project/index.js
+++ cache-project/index.js
@@ -282,13 +282,13 @@ Select operation:
 `);
 		switch (Number(readline.question('Enter choice: '))) {
 			case 1: {
-				const address = parseInt(readline.question('Enter address (hexa): '), 16);
+				const address = parseInt(readline.question('Enter address: '), 10);
 				operations.read(address);
 				break;
 			}
 
 			case 2: {
-				const address = parseInt(readline.question('Enter address (hexa): '), 16);
+				const address = parseInt(readline.question('Enter address: '), 10);
 				const data = Number(readline.question('Enter data: '));
 				operations.write(data, address);
 				break;
